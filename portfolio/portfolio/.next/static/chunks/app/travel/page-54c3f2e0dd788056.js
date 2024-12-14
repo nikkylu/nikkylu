013f710edcabfac6a08f6328c42dc58b9dabfac7
nikkylu <nikkylu@foxmail.com>
2024-12-14 02:07:39 -0500

    Clean setup without embedded repository

---
+++ portfolio/portfolio/.next/static/chunks/app/travel/page-54c3f2e0dd788056.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[252,366,567,678,697,811,974],{5797:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,7970,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[173,970,441,517,358],()=>s(5797)),_N_E=e.O()}]);
